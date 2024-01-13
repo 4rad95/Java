a0624799eaa63af5a3d7857b364e2496b25ff22c
4rad95 <148912953+4rad95@users.noreply.github.com>
2024-01-13 14:57:56 +0100

    Add files via upload

---
+++ HW9/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static org.example.Const.*;
+import static org.example.Lottery.startLottery;
+import static org.example.Util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+// Увеличьте одновременное количество игроков до трех.
+// Три игрока вводят номера для игры.
+// Лотерея играет один раз.
+
+        startLottery();
+    }
+
+}
